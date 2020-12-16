5c830dd918fdffbf3ec6ae5a5e0536893bf4b7a4
Ashish maan <ashishmaan22@gmail.com>
2020-12-16 13:49:49 +0530

    added

---
+++ LeetCode/Linked_list/Remove Linked List Elements.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while(head != null && head.val == val){
+            head = head.next;
+        }
+        ListNode l = head;
+        while(l != null && l.next != null){
+            if(l.next.val == val){
+                l.next = l.next.next;
+            }else{
+                l = l.next;
+            }
+
+        }
+        return head;
+    }
+}

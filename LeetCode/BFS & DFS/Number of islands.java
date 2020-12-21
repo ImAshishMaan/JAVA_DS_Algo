5b3f8d1d364d83505e2325a1d1e94f134bcf2987
Ashish maan <ashishmaan22@gmail.com>
2020-12-21 09:01:59 +0530

    created

---
+++ LeetCode/BFS & DFS/Number of islands.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null ){
+            return 0;
+        }
+        int iland = 0;
+        for(int i=0 ; i<grid.length ; i++){
+            for(int j= 0 ; j<grid[i].length ; j++){
+                if(grid[i][j] == '1'){
+                    iland += find(grid, i, j);
+                }
+            }
+        }
+        return iland;
+    }
+    public int find(char[][] grid, int i, int j){
+        if(i<0 || i >= grid.length || j <0 || j >= grid[i].length || grid[i][j] == '0'){
+            return 0;
+        }
+        grid[i][j] = '0';
+        find(grid, i+1, j);
+        find(grid, i-1, j);
+        find(grid, i, j+1);
+        find(grid, i, j-1);
+        return 1;
+    }
+}

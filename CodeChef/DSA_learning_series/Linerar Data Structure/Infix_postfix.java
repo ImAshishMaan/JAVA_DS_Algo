854d666198b2a1552733ff49e2956c8a407b49a6
ImAshishMaan <ashishmaan22@gmail.com>
2021-01-14 19:58:37 +0530

    Create Infix_postfix.java

---
+++ CodeChef/DSA_learning_series/Linerar Data Structure/Infix_postfix.java
@@ -0,0 +1,15 @@
+package DSA_learning_series_week2;
+
+import java.util.*;
+class Infix_postfix{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while(T-- > 0){
+            int N = sc.nextInt();
+            String S = sc.next();
+            
+        }
+   }
+}

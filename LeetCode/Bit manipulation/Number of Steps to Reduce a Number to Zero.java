bc4db121696c221097f99e3e497a54f5fcebe11d
ImAshishMaan <ashishmaan22@gmail.com>
2021-07-16 08:19:08 +0530

    Create Number of Steps to Reduce a Number to Zero.java

---
+++ LeetCode/Bit manipulation/Number of Steps to Reduce a Number to Zero.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int s = 0;
+        while(num > 0){
+            if(num%2 == 0){
+                num = num >> 1;
+                s++;
+            }else{
+                num = num ^ 1;
+                s++;
+            }
+            
+        }
+        return s;
+    }
+}
